530d3b29c313b4fee4582615db3a894be24491e3
Prayag Patel <prayagp8@gmail.com>
2022-07-21 23:15:19 +0530

    landing page mid section done

--- day3/script/login.js
+++ day3/script/login.js
@@ -1,34 +1,35 @@
-let login=()=>{
-    let number = document.getElementById("mobileNumber").value
-    let password = document.getElementById("password").value
-   
-    if(number.length!=10){
-      return alert("Please enter a valid Number")
-    }
-  
-  
-    if(validateLogin(number,password)){
+let login = () => {
+  let number = document.getElementById("mobileNumber").value
+  let password = document.getElementById("password").value
+
+  if (number.length != 10) {
+    return alert("Please enter a valid Number")
+  }
+
+
+  if (validateLogin(number, password)) {
 
     alert("login successful!")
     window.location.href = "./landing.html"
-  }else{
+  } else {
     alert("login failed!")
   }
 
- }
+}
 
 
- function validateLogin(number,password){
-    let userArr = JSON.parse(localStorage.getItem("userArr"))
-    console.log(userArr)
+function validateLogin(number, password) {
+  let userArr = JSON.parse(localStorage.getItem("userArr"))
+  console.log(userArr)
 
-    let filtered = userArr.filter(function(ele){
-        return ele.number == number && ele.password == password
-    })
-    if(filtered.length>0){
-        localStorage.setItem("currUser",JSON.stringify(filtered))
-        return true
-    }else{
-        return false
-    }
- }
+  let filtered = userArr.filter(function (ele) {
+    return ele.number == number && ele.password == password
+  })
+  if (filtered.length > 0) {
+    localStorage.setItem("currUser", JSON.stringify(filtered))
+    return true
+  } else {
+    return false
+  }
+}
+
